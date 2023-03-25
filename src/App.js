9472851e6611943a97c5e67709c881c6d492632a
Jiteshkumar yadav <9766jitesh@gmail.com>
2023-03-25 08:29:57 +0530

    home page created

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import HomePage from './components/HomePage';
 import ContestProvider from './contextApi/contextApi';
+import { RouterProvider, Routes, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <ContestProvider>
+  function App() {
+    return (
+      <div className="App">
+        <ContestProvider>
+          <Routes>
 
-      <HomePage></HomePage>
-      </ContestProvider>
-    </div>
-  );
-}
+            <Route path="/homepage" element={<HomePage />} />
+            <Route path="/homepage/:id" element={<HomePage />} />
+          </Routes>
+        </ContestProvider>
+      </div>
+    );
+  }
 
 export default App;
